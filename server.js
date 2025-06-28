4e674bbfbee83b7d91f3ad999d5b838ad04af931
Yash <yashagr.805@gmail.com>
2025-06-28 16:43:15 +0530

    JSON2JSON

--- server.js
+++ server.js
@@ -5,25 +5,12 @@ const { Parser } = require('json2csv');
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 app.use(bodyParser.json());
 
 app.post('/', (req, res) => {
-  const jsonData = req.body;
-
-  if (!Array.isArray(jsonData)) {
-    return res.status(400).send('Invalid JSON: expected an array of objects');
-  }
-
-  try {
-    const json2csvParser = new Parser();
-    const csv = json2csvParser.parse(jsonData);
-    res.header('Content-Type', 'text/csv');
-    res.attachment('data.csv');
-    return res.send(csv);
-  } catch (err) {
-    console.error('Error converting to CSV:', err);
-    return res.status(500).send('Failed to convert JSON to CSV');
-  }
+  res.send(req.body)
 });
 
 app.listen(port, () => {
